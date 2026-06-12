915f6518e546453cd126d41d3c8087d3b430c319
Your Name <choyousakphea@gmail.com>
2026-06-12 08:38:37 +0700

    first function added

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+string num1() {
+    string x = "hello world";
+    return x;
+}
+int main() {
+
+    cout << num1() << endl;
+    
+    
+    return 0;
+};
